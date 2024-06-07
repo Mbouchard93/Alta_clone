ffb5845795634236732dbdb9bd546d5a83148f93
Mbouchard93 <mariiane.bouchard12@gmail.com>
2024-06-07 18:34:03 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./src/**/*.{js,html}",
+  ],
+  theme: {
+    colors : {
+      'blue' : '#4f46e5',
+      'dark' : '#111827',
+      'withe' : '#fff'
+    },
+    fontFamily : {
+      'poppins' : [' "Poppins", sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
